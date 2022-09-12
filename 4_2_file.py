d2abe0461514654b8837a69e43770bd1b433089e
blackJJW <jjinwoo92@gmail.com>
2022-09-12 20:56:21 +0900

    생뢀코딩 python 입문

---
+++ 4_2_file.py
@@ -0,0 +1,10 @@
+print('Hello world')
+print('''
+Hello
+World
+''')
+
+print("'1' + '1'", '1'+'1')
+print('Hello World' * 1000)
+print("len('Hello World'*1000)", len('Hello World'*1000))
+print("'Hello world'.replace('world', 'universe')", 'Hello world'.replace('world', 'universe'))
---
+++ 6_1_file.py
@@ -0,0 +1,8 @@
+price = 10000
+vat_rate = 0.1
+vat = price * vat_rate
+
+print(vat)
+
+name = input('name: ')
+print('hi,'+ name + ' ..... bye, ' + name +'. ')
